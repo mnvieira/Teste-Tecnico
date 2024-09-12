b3acd5ce2ef0a14c31001cf45b48fc9f07e92093
mnvieira <110779046+mnvieira@users.noreply.github.com>
2024-09-12 15:55:06 -0300

    Create teste_tecnico_5_pegunta .py

---
+++ teste_tecnico_5_pegunta .py
@@ -0,0 +1,10 @@
+teste_tecnico_5_pegunta .py
+# String de exemplo
+string = input("Informe uma string para inverter: ")
+
+# Invertendo manualmente
+string_invertida = ''
+for char in string:
+    string_invertida = char + string_invertida
+
+print("String invertida:", string_invertida
