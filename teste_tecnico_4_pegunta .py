cc95094f3be9cb2d922272601cb26eb7273f5fce
mnvieira <110779046+mnvieira@users.noreply.github.com>
2024-09-12 15:54:02 -0300

    Create teste_tecnico_4_pegunta .py

---
+++ teste_tecnico_4_pegunta .py
@@ -0,0 +1,16 @@
+teste_tecnico_4_pegunta .py
+faturamento_estados = {
+    "SP": 67836.43,
+    "RJ": 36678.66,
+    "MG": 29229.88,
+    "ES": 27165.48,
+    "Outros": 19849.53
+}
+
+# Somar o total de faturamento
+faturamento_total = sum(faturamento_estados.values())
+
+# Calcular e mostrar o percentual de cada estado
+for estado, valor in faturamento_estados.items():
+    percentual = (valor / faturamento_total) * 100
+    print(f"{estado}: {percentual:.2f}%")
